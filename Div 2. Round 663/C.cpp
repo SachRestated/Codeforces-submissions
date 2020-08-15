d70d483f7d5cc47d08251134d78611045d53d9ec
SachRestated <sachinvashisht1999@gmail.com>
2020-08-15 17:05:35 +0530

    Div 2 #663

--- Div 2. Round 663/C.cpp
+++ Div 2. Round 663/C.cpp
@@ -27,38 +27,56 @@ using namespace __gnu_pbds;
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
+const int MAXN = 1e6 + 10;
+
+int sub(int a,int b,int p=MOD){return ((a%p)-(b%p)+p)%p;}
+int mult(int a,int b, int p=MOD) {return ((a%p)*(b%p))%p;}
+int add(int a, int b, int p=MOD) {return (a%p + b%p)%p;}
+int fpow(int n, int k, int p = MOD) {int r = 1; while(k > 0) {if (k & 1) r = r * n%p; n = n * n%p; k = k >> 1;} return r;}
+inline int powr(int a, int b) {
+    int x = 1;
+    while (b) {
+        if (b & 1) x *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return x;
+}
+int inv(int a, int p = MOD) {return fpow(a, p - 2, p);}
+int fdiv(int a,int b,int p=MOD){
+    int yinv=inv(b);
+    int ans=(a*yinv)%p;
+    return ans;
+}
+ 
+int _pow(int p, int q){
+    int res = 1;
+    while(q){
+        if(q&1) res = (res*p)%MOD;
+        p = (p*p)%MOD;
+        q = q>>1;
+    }
+    return (res+MOD)%MOD;
+}
+v1d fact(1e6 + 10, 1);
+
+void preprocess() {
+    for(int i=1; i<MAXN; ++i) {
+        fact[i] = fact[i - 1] * i;
+        fact[i] %= MOD;
+    }
+}
 
 void SachRestated() {
-    int n, m; cin >> n >> m;
-    v1d a(n);
-    v1d b(m);
-    
-    for(int i=0; i<n; ++i) cin >> a[i];
-    for(int i=0; i<m; ++i) cin >> b[i];
-    for(int i=0; i<512; ++i) {
-        bool ok = true;
-        for(int j=0; j<n && ok; ++j) {
-            bool x = false;
-            for(int k=0; k<m && ok && !x; ++k) {
-                int p = i;
-                int q = a[j] & b[k];
-                p |= q;
-                if(p == i) {
-                   x = true; 
-                }
-            }
-            ok &= x;
-        }
-        if(ok) {
-            cout << i << endl; return;
-        }
-    } 
+    int n; cin >> n;
+    cout << sub(fact[n], _pow(2, n - 1)); 
 
 }
 
 int32_t main() {
     fio
     int t = 1;
+    preprocess();
     // cin >> t;
     while(t--) {
       SachRestated();  
