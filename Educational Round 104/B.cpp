d0b28d33db99fd060ce83671ee6ede63a2c22e76
SachRestated <sachinvashisht1999@gmail.com>
2021-02-16 16:52:29 +0530

    Div 2 #701

---
+++ Educational Round 104/B.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define int long long
+#define sz(v) (int)v.size()
+#define pr pair<int, int>
+#define v1d vector<int>
+#define mp make_pair
+#define pb push_back
+#define v1p vector<pr>
+#define vb vector<bool>
+#define v2d vector<vector<int>>
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+#define S(v) sort(v.begin(), v.end())
+#define RS(v) sort(v.rbegin(), v.rend())
+#define eb emplace_back
+#define ld long double
+#define endl "\n"
+#define MOD 1000000007
+#define pqx priority_queue <int>
+#define pqn priority_queue <int, v1d, greater<int> >
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define OJ freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+
+void SachRestated() {
+    int n, k; cin >> n >> k;
+    if(n % 2 == 0) {
+        int p = k % n;
+        if(p == 0) p += n;
+        cout << p << endl;
+    } else {
+        if(k <= n / 2) {
+            cout << k << endl ;
+            return ;
+        }
+        int acc = 1;
+        int p = (n + 1) / 2;
+        int x = k - (p);
+        acc = 1 + (x / (p - 1));
+        int ans = (k + acc) % n;
+        if(ans == 0) ans = n;
+        cout << ans << endl;
+
+        
+    }
+}
+
+int32_t main() {
+    fio
+
+    clock_t z = clock();
+    //OJ;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        SachRestated();  
+    }
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << '\n';
+}
